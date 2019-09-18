74da3a728b2cd0c094d1e98b43c8dcc137ccf7c5
DaishPeter <49520497+DaishPeter@users.noreply.github.com>
2019-09-18 23:13:24 +0100

    Added UI Initialisation on Login Form

--- PoeTree/PoeTree/PoeTreeMain.Designer.cs
+++ PoeTree/PoeTree/PoeTreeMain.Designer.cs
@@ -28,27 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbl_HelloWorld = new System.Windows.Forms.Label();
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.usernameTxt = new System.Windows.Forms.TextBox();
+            this.usernameConfirmButton = new System.Windows.Forms.Button();
+            this.welcomeUserLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lbl_HelloWorld
+            // titleLbl
             // 
-            this.lbl_HelloWorld.AutoSize = true;
-            this.lbl_HelloWorld.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_HelloWorld.Location = new System.Drawing.Point(260, 112);
-            this.lbl_HelloWorld.Name = "lbl_HelloWorld";
-            this.lbl_HelloWorld.Size = new System.Drawing.Size(153, 31);
-            this.lbl_HelloWorld.TabIndex = 0;
-            this.lbl_HelloWorld.Text = "Hello World";
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Location = new System.Drawing.Point(329, 44);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(42, 13);
+            this.titleLbl.TabIndex = 0;
+            this.titleLbl.Text = "titleLBL";
+            // 
+            // usernameTxt
+            // 
+            this.usernameTxt.Location = new System.Drawing.Point(302, 130);
+            this.usernameTxt.Name = "usernameTxt";
+            this.usernameTxt.Size = new System.Drawing.Size(100, 20);
+            this.usernameTxt.TabIndex = 1;
+            // 
+            // usernameConfirmButton
+            // 
+            this.usernameConfirmButton.Location = new System.Drawing.Point(312, 200);
+            this.usernameConfirmButton.Name = "usernameConfirmButton";
+            this.usernameConfirmButton.Size = new System.Drawing.Size(75, 23);
+            this.usernameConfirmButton.TabIndex = 2;
+            this.usernameConfirmButton.Text = "Confirm";
+            this.usernameConfirmButton.UseVisualStyleBackColor = true;
+            this.usernameConfirmButton.Click += new System.EventHandler(this.usernameConfirmButton_Click);
+            // 
+            // welcomeUserLbl
+            // 
+            this.welcomeUserLbl.AutoSize = true;
+            this.welcomeUserLbl.Location = new System.Drawing.Point(312, 279);
+            this.welcomeUserLbl.Name = "welcomeUserLbl";
+            this.welcomeUserLbl.Size = new System.Drawing.Size(85, 13);
+            this.welcomeUserLbl.TabIndex = 3;
+            this.welcomeUserLbl.Text = "welcomeUserLbl";
             // 
             // PoeTreeMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lbl_HelloWorld);
+            this.Controls.Add(this.welcomeUserLbl);
+            this.Controls.Add(this.usernameConfirmButton);
+            this.Controls.Add(this.usernameTxt);
+            this.Controls.Add(this.titleLbl);
             this.Name = "PoeTreeMain";
             this.Text = "PoeTree";
+            this.Load += new System.EventHandler(this.PoeTreeMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,7 +88,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lbl_HelloWorld;
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.TextBox usernameTxt;
+        private System.Windows.Forms.Button usernameConfirmButton;
+        private System.Windows.Forms.Label welcomeUserLbl;
     }
 }
 
--- PoeTree/PoeTree/PoeTreeMain.cs
+++ PoeTree/PoeTree/PoeTreeMain.cs
@@ -10,11 +10,48 @@ using System.Windows.Forms;
 
 namespace PoeTree
 {
+
     public partial class PoeTreeMain : Form
     {
+
+        /*########### UI ELEMENTS ############
+         * private Label titleLbl; -- The program's title.
+         * private TextField usernameTxt; -- The name the user enters.
+         * private Button usernameConfirmButton; -- Button to confirm user's name.
+         * private Label welcomeUserLbl; -- Label to display a welcome message to user.
+        */
+
+        //########### UI Attributes ############
+        private const String PROG_TITLE = "PoeTree"; //Title of the program PoeTree - a Poetry app.
+        private const String BTN_CONFIRM_MESSAGE = "Confirm";
+        private const String LBL_WELCOME_MESSAGE = ", hello and welcome to PoeTree!";
+
         public PoeTreeMain()
         {
             InitializeComponent();
+        }
+
+        private void PoeTreeMain_Load(object sender, EventArgs e)
+        {
+            InitialiseUI();
+        }
+
+        /*
+         *Initialises the UI to display the correct values to the user upon startup. 
+         */
+        private void InitialiseUI()
+        {
+            titleLbl.Text = PROG_TITLE;
+            usernameConfirmButton.Text = BTN_CONFIRM_MESSAGE;
+            welcomeUserLbl.Text = "";
+        }
+
+        /*
+         *Updates the welcome message to display the username previously entered. 
+         */
+        private void usernameConfirmButton_Click(object sender, EventArgs e)
+        {
+            welcomeUserLbl.Text = usernameTxt.Text + LBL_WELCOME_MESSAGE;
         }
     }
 }
